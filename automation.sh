6eb3e2f57c1079e8c30a0705b712655ac7bf04b8
root <root@ip-172-31-21-215.ec2.internal>
2021-06-01 08:49:47 +0000

    updated automation.sh -- v2.0

--- automation.sh
+++ automation.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 current_time=$(date +"%d%m%y-%H%M%S")
+kb=K
 
 sudo apt update -y
 
@@ -13,8 +14,8 @@ else
   sudo apt install apache2 -y
 fi
 
-sleep 5
 
+sleep 5
 ps cax | grep -i 'apache2' &> /dev/null
 if [ $? -eq 0 ]
 then
@@ -23,24 +24,86 @@ else
  echo "**apache2 is not running**"
 fi
 
+
 sleep 5
-
 sudo systemctl enable apache2
-
 service apache2 status &> /dev/null
-if [$? -eq 0]
+
+if [ $? -eq 0 ]
 then 
  echo "**apache2 is enabled**"
 else
  echo "**apache2 is not enabled**"
 fi
 
-sleep 5
 
+sleep 5
 echo "**capturing apache2 logs...**"
 tar -zcvf /tmp/Anupam-httpd-logs-$current_time.tar /var/log/apache2/*.log
+size_of_log=`wc -l /tmp/Anupam-httpd-logs-$current_time.tar | awk '{print $1}'`
 
 sleep 5
 echo "**copying the logs to s3**"
 aws s3 cp /tmp/Anupam-httpd-logs-$current_time.tar s3://upgrad-anupamdutta/Anupam-httpd-logs-$current_time.tar
 
+
+
+sleep 5
+echo "**creating and updating inventory.html**"
+
+FILE=/var/www/html/inventory.html
+if [ -f "$FILE" ]; then
+	    echo "**$FILE exists.**"
+	    echo "<html>
+	              <body>
+			  <table style="width:40%">
+			     <tr>
+			       <td>httpd</td>
+			       <td>$current_time</td>
+			       <td>tar</td>
+			       <td>$size_of_log$kb</td>
+			     </tr>
+			  </table>
+                      </body>
+		   </html>" >> /var/www/html/inventory.html   
+                             
+else 
+	    echo "**$FILE does not exist.Creating new file..**"
+            touch /var/www/html/inventory.html
+            echo "<html>
+                  <head>
+                     <title>
+                        Inventory
+	             </title>
+	          </head>
+	          <body>
+		    <table style="width:40%">
+		       <tr>
+		              <th>Log Type</th>
+			      <th>Date Created</th>
+			      <th>Type</th>
+		              <th>Size</th>
+			</tr>
+			<tr>
+			      <td>httpd</td>
+			      <td>$current_time</td>
+			      <td>tar</td>
+			      <td>$size_of_log$kb</td>
+			</tr>
+		    </table>
+		  </body>
+                  </html>" >> /var/www/html/inventory.html
+fi
+
+sleep 5
+echo "**copying inventory.html to s3**"
+aws s3 cp /var/www/html/inventory.html s3://upgrad-anupamdutta
+
+sleep 5
+echo "**creating cron job..**"
+curr_path=$(pwd)
+echo $curr_path
+cd /etc/cron.d/
+sudo touch /etc/cron.d/automation
+sudo echo "0 20 * * * root /root/Automation_Project-/automation.sh" > /etc/cron.d/automation
+sudo chmod 600 /etc/cron.d/automation
